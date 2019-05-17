e0d6512ad7ecec518d760560c6e3e1f9f426c91a
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-05-17 23:43:56 +0200

    [WIP] Fix linefeed.

--- src/test/java/be/yildizgames/module/color/ColorTest.java
+++ src/test/java/be/yildizgames/module/color/ColorTest.java
@@ -27,10 +27,7 @@ package be.yildizgames.module.color;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Grégory Van den Borre
