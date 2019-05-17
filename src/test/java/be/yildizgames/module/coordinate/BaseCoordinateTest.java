2f7cfe75d06da78047bd0ff7fa537886c9d5820b
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-05-17 23:44:44 +0200

    [WIP] Fix linefeed.

--- src/test/java/be/yildizgames/module/coordinate/BaseCoordinateTest.java
+++ src/test/java/be/yildizgames/module/coordinate/BaseCoordinateTest.java
@@ -26,9 +26,7 @@ package be.yildizgames.module.coordinate;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Grégory Van den Borre
